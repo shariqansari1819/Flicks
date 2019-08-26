e7ee47d0f545b409858a869e45ab59ff5740cd7c
Shariq Ansari <shariqansari1819@gmail.com>
2019-08-27 00:02:11 +0500

    Added favorite tv shows list.

---
+++ app/src/main/java/com/codebosses/flicks/adapters/favorite/FavoriteTvShowsAdapter.java
@@ -0,0 +1,132 @@
+package com.codebosses.flicks.adapters.favorite;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.codebosses.flicks.R;
+import com.codebosses.flicks.database.entities.MovieEntity;
+import com.codebosses.flicks.database.entities.TvShowEntity;
+import com.codebosses.flicks.endpoints.EndpointUrl;
+import com.codebosses.flicks.pojo.eventbus.EventBusMovieClick;
+import com.codebosses.flicks.pojo.eventbus.EventBusTvShowsClick;
+import com.codebosses.flicks.utils.FontUtils;
+
+import org.greenrobot.eventbus.EventBus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class FavoriteTvShowsAdapter extends RecyclerView.Adapter<FavoriteTvShowsAdapter.MoviesHolder> {
+
+    //    Android fields....
+    private Context context;
+    private LayoutInflater layoutInflater;
+
+    //    Font fields....
+    private FontUtils fontUtils;
+
+    //    Instance fields....
+    private List<TvShowEntity> moviesResultArrayList = new ArrayList<>();
+    private String movieType;
+
+    public FavoriteTvShowsAdapter(Context context, List<TvShowEntity> moviesResultArrayList, String movieType) {
+        this.context = context;
+        fontUtils = FontUtils.getFontUtils(context);
+        this.moviesResultArrayList = moviesResultArrayList;
+        layoutInflater = LayoutInflater.from(context);
+        this.movieType = movieType;
+    }
+
+    @NonNull
+    @Override
+    public MoviesHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = layoutInflater.inflate(R.layout.row_tv_shows, parent, false);
+        return new MoviesHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MoviesHolder holder, int position) {
+        TvShowEntity moviesResult = moviesResultArrayList.get(position);
+        if (moviesResult != null) {
+            String posterPath = moviesResult.getPosterPath();
+            String title = moviesResult.getName();
+            String releaseDate = moviesResult.getFirstAirDate();
+            double voteAverage = moviesResult.getVoteAverage();
+            int voteCount = 0;
+            if (posterPath != null && !posterPath.equals(""))
+                Glide.with(context)
+                        .load(EndpointUrl.POSTER_BASE_URL + "/" + posterPath)
+                        .apply(new RequestOptions().placeholder(R.drawable.zootopia_thumbnail))
+                        .thumbnail(0.1f)
+                        .into(holder.imageViewThumbnail);
+            if (title != null && !title.isEmpty()) {
+                holder.textViewMovieTitle.setText(title);
+            } else {
+                holder.textViewMovieTitle.setText("No Title Found");
+            }
+            if (releaseDate != null && !releaseDate.isEmpty())
+                holder.textViewMovieYear.setText(releaseDate);
+            else {
+                holder.textViewMovieYear.setText("No Release Date");
+            }
+            holder.textViewRatingCount.setText(String.valueOf(voteAverage));
+            holder.textViewVoteCount.setText(String.valueOf(voteCount));
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return moviesResultArrayList.size();
+    }
+
+    class MoviesHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        @BindView(R.id.imageViewThumbnailTvShowsRow)
+        ImageView imageViewThumbnail;
+        @BindView(R.id.textViewTitleTvShowsRow)
+        TextView textViewMovieTitle;
+        @BindView(R.id.textViewYearTvShowsRow)
+        TextView textViewMovieYear;
+        @BindView(R.id.textViewAudienceTvShowsRow)
+        TextView textViewRatingText;
+        @BindView(R.id.textViewRatingTvShowsRow)
+        TextView textViewRatingCount;
+        @BindView(R.id.textViewVotesTvShowsRow)
+        TextView textViewVoteCount;
+        @BindView(R.id.textViewVotesCountTvShowsRow)
+        TextView textViewVoteCountText;
+
+        MoviesHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+
+            fontUtils.setTextViewRegularFont(textViewMovieTitle);
+            fontUtils.setTextViewLightFont(textViewMovieYear);
+            fontUtils.setTextViewLightFont(textViewRatingText);
+            fontUtils.setTextViewLightFont(textViewVoteCountText);
+            fontUtils.setTextViewRegularFont(textViewVoteCount);
+            fontUtils.setTextViewRegularFont(textViewRatingCount);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            EventBus.getDefault().post(new EventBusTvShowsClick(getAdapterPosition(), movieType));
+        }
+
+    }
+
+}
--- app/src/main/java/com/codebosses/flicks/fragments/favorites/FavoriteTvShowsFragment.java
+++ app/src/main/java/com/codebosses/flicks/fragments/favorites/FavoriteTvShowsFragment.java
@@ -1,28 +1,123 @@
 package com.codebosses.flicks.fragments.favorites;
 
-
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.budiyev.android.circularprogressbar.CircularProgressBar;
 import com.codebosses.flicks.R;
+import com.codebosses.flicks.activities.MoviesDetailActivity;
+import com.codebosses.flicks.activities.TvShowsDetailActivity;
+import com.codebosses.flicks.adapters.favorite.FavoriteMoviesAdapter;
+import com.codebosses.flicks.adapters.favorite.FavoriteTvShowsAdapter;
+import com.codebosses.flicks.database.DatabaseClient;
+import com.codebosses.flicks.database.entities.MovieEntity;
+import com.codebosses.flicks.database.entities.TvShowEntity;
+import com.codebosses.flicks.endpoints.EndpointKeys;
+import com.codebosses.flicks.pojo.eventbus.EventBusMovieClick;
+import com.codebosses.flicks.pojo.eventbus.EventBusTvShowsClick;
+import com.codebosses.flicks.utils.ValidUtils;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import jp.wasabeef.recyclerview.animators.FadeInDownAnimator;
 
 public class FavoriteTvShowsFragment extends Fragment {
 
+    //    Android fields....
+    @BindView(R.id.circularProgressBarFavoriteTvShows)
+    CircularProgressBar circularProgressBarFavoriteTvShows;
+    @BindView(R.id.recyclerViewFavoriteTvShows)
+    RecyclerView recyclerViewFavoriteTvShows;
+
+    //    Adapter fields....
+    private FavoriteTvShowsAdapter favoriteTvShowsAdapter;
+    private List<TvShowEntity> tvShowEntityArrayList = new ArrayList<>();
 
     public FavoriteTvShowsFragment() {
 
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_favorite_tv_shows, container, false);
+        View view = inflater.inflate(R.layout.fragment_favorite_tv_shows, container, false);
+        ButterKnife.bind(this, view);
+
+        recyclerViewFavoriteTvShows.setLayoutManager(new LinearLayoutManager(getActivity()));
+        favoriteTvShowsAdapter = new FavoriteTvShowsAdapter(getActivity(), tvShowEntityArrayList, EndpointKeys.FAVORITE_TV_SHOW);
+        recyclerViewFavoriteTvShows.setAdapter(favoriteTvShowsAdapter);
+        recyclerViewFavoriteTvShows.setItemAnimator(new FadeInDownAnimator());
+        if (recyclerViewFavoriteTvShows.getItemAnimator() != null)
+            recyclerViewFavoriteTvShows.getItemAnimator().setAddDuration(500);
+        loadFavoriteTvShow();
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (!EventBus.getDefault().isRegistered(this)) {
+            EventBus.getDefault().register(this);
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (EventBus.getDefault().isRegistered(this)) {
+            EventBus.getDefault().unregister(this);
+        }
+    }
+
+    private void loadFavoriteTvShow() {
+        new GetAllTvShowsTask().execute();
+    }
+
+    class GetAllTvShowsTask extends AsyncTask<Void, Void, List<TvShowEntity>> {
+
+        @Override
+        protected List<TvShowEntity> doInBackground(Void... voids) {
+            return DatabaseClient.getDatabaseClient(getActivity()).getFlicksDatabase().getFlicksDao().getAllFavoriteTvShows();
+        }
+
+        @Override
+        protected void onPostExecute(List<TvShowEntity> aVoid) {
+            super.onPostExecute(aVoid);
+            if (aVoid.size() > 0) {
+                for (int i = 0; i < aVoid.size(); i++) {
+                    tvShowEntityArrayList.add(aVoid.get(i));
+                    favoriteTvShowsAdapter.notifyItemInserted(i);
+                }
+            }
+        }
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void eventBusTvShowsClick(EventBusTvShowsClick eventBusTvShowsClick) {
+        if (eventBusTvShowsClick.getTvShowType().equals(EndpointKeys.FAVORITE_TV_SHOW) && ValidUtils.isNetworkAvailable(getContext())) {
+            Intent intent = new Intent(getActivity(), TvShowsDetailActivity.class);
+            intent.putExtra(EndpointKeys.TV_ID, tvShowEntityArrayList.get(eventBusTvShowsClick.getPosition()).getId());
+            intent.putExtra(EndpointKeys.TV_NAME, tvShowEntityArrayList.get(eventBusTvShowsClick.getPosition()).getName());
+            intent.putExtra(EndpointKeys.RATING, tvShowEntityArrayList.get(eventBusTvShowsClick.getPosition()).getVoteAverage());
+            startActivity(intent);
+        }
     }
 
 }
